0caf90864116cff2f0d5c3fe889527ad9f34f8c7
altrono <hbotema16@gmail.com>
2026-08-04 21:59:38 +0200

    new fix

--- app/(admin-dashboard)/admin/page.tsx
+++ app/(admin-dashboard)/admin/page.tsx
@@ -1,6 +1,9 @@
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
+
+export const dynamic = "force-dynamic";
+
 export default async function AdminPage() {
   const [products, orders] = await Promise.all([
     prisma.product.count(),
